9a4b3c839a2325aa2743a68672486a16ae9ac2ed
Mauricio <Mauricio@FamiliaAlvaro>
2017-02-24 13:19:56 -0600

    Inicio del juego

---
+++ src/picasyfijas/PicasYFijas.java
@@ -0,0 +1,31 @@
+/**
+ * Juego de Picas y fijas
+ * Descripción: Recibe los operando y la operacion y hace le calculo
+ * Fecha:23 de febrero del 2017
+ * Autor:Jose Solano Montoya y Adrián Obando Leitón
+ * Fecha de modificación:24-02-17
+ * Modificado por: Adrián Obando Leitón
+ */
+package picasyfijas;
+
+
+public class PicasYFijas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Generar Ramdom
+        generateRam();
+    }
+    public static void generateRam ()
+    {
+        
+    }
+    public int  numeroPicas(int numCompu, int numUsu)
+    {
+        int picas=0;
+        
+        return picas;
+    }
+}
